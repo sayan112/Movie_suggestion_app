1cc0af913e09735a0107536e9cc5ec9758768620
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-18 21:29:45 +0530

    "Done Final Touch"

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles({
     width: "100%",
     position: "fixed",
     bottom: 0,
-    backgroundColor: "#2d313a",
+    backgroundColor: "#1a2a48",
     zIndex: 100,
   },
 });
